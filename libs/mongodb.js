936557eedc5213bbd4cf3ddd3d036cf22e67fdb0
samicelus <samicelus@hotmail.com>
2016-08-11 13:48:35 +0800

    release

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -3,15 +3,23 @@ var Promise = require('bluebird');
 Promise.promisifyAll(mongoose);
 var util = require('./utils.js');
 
+var connec;
 var mongoConfig = require(util.configDir + '/mongodb.json').dataServer;
 if(mongoConfig.user){
-	mongoose.connect('mongodb://' + mongoConfig.user + ':' + mongoConfig.pwd + '@' + mongoConfig.host + ':' + mongoConfig.port + '/' + mongoConfig.db);
+	var url = 'mongodb://' + mongoConfig.user + ':' + mongoConfig.pwd + '@' + mongoConfig.host + ':' + mongoConfig.port + '/' + mongoConfig.db + '?authMechanism=MONGODB-CR';
+	console.log('trying to connect: '+url);
+	var opts = {
+    auth: {
+        authMechanism: 'MONGODB-CR'
+    	}
+	};
+	connec = mongoose.createConnection(url,opts);
 	}else{
 		mongoose.connect('mongodb://' + mongoConfig.host + ':' + mongoConfig.port + '/' + mongoConfig.db);
 		}
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console,'connection error:'));
+
+connec.on('error', console.error.bind(console,'connection error:'));
 module.exports = mongoose;
 
 
